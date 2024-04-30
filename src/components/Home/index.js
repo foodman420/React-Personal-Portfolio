c69b6614621d462ee2a3ca6f3cb34ecb826601cb
Foodman420 <jacksgoodman3@gmail.com>
2024-04-30 15:01:04 +0100

    Merge branch 'master' of https://github.com/foodman420/React-Personal-Portfolio

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,11 +6,13 @@ import React, { useState, useEffect } from 'react';
 import userEvent from '@testing-library/user-event';
 import Logo from './Logo';
 
+
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['a', 'c', 'k']
-    const jobArray = []
-
+    const jobArray = ['B', 'S', 'c', ' ', 'C', 'o', 'm', 'p', 'u', 't', 'e', 'r', ' ']
+    const scienceArray = ['S', 'c', 'i', 'e', 'n', 'c', 'e']
+    const studentArray = ['S', 't', 'u', 'd', 'e', 'n', 't']
     useEffect(() => {
         const timer = setTimeout(() => {
             setLetterClass('text-animate-hover');
@@ -34,6 +36,18 @@ const Home = () => {
                         strArray={nameArray}
                         idx={15} />
                     <br />
+                    <br />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={20} />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={scienceArray}
+                        idx={25} />
+
+                    <br />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={studentArray}
+                        idx={29} />
 
                 </h1>
                 <h2>
